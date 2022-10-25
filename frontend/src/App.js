956ca3c4f7b3011684dbe092cbcd519de7364fdf
Gabriel <brysiekholub@gmail.com>
2022-10-25 21:07:23 +0200

    get single data from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+ 
+  const options = {
+    method: 'GET',
+    url: 'https://alpha-vantage.p.rapidapi.com/query',
+    params: {
+      from_symbol: 'EUR',
+    function: 'FX_DAILY',
+    to_symbol: 'USD',
+    outputsize: 'compact',
+    datatype: 'json'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'f63b5994eemsh8177fbc473e2d5ap11b741jsn020b4cb78cb0',
+      'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+    }
+  };
+ 
+  axios.request(options).then(function (response) {
+     
+      console.log(response.data);
+     for(let date in response.data["Time Series FX (Daily)"]){
+      console.log(date)
+      console.log(response.data["Time Series FX (Daily)"][date]["1. open"])
+      console.log(response.data["Time Series FX (Daily)"][date]["2. high"])
+      console.log(response.data["Time Series FX (Daily)"][date]["3. low"])
+      console.log(response.data["Time Series FX (Daily)"][date]["4. close"])
+
+     }
+
+    
+
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
